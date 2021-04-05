20bf1aa4c91d22685b732bea24ba5969fffe0bf8
Manolo <manuelgmaroto@gmail.com>
2021-04-05 20:29:25 +0200

    Added some tests

--- src/app/services/f1-stats.service.spec.ts
+++ src/app/services/f1-stats.service.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed } from '@angular/core/testing';
-
+import { fakeAsync, inject, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { F1StatsService } from './f1-stats.service';
 
 describe('F1StatsService', () => {
   let service: F1StatsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientModule],
+      providers: [F1StatsService] 
+    });
     service = TestBed.inject(F1StatsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get current drivers stats', fakeAsync(() => {
+    const service: F1StatsService = TestBed.inject(F1StatsService);
+    service.getCurrentDrivers().subscribe(
+      (res) => expect(res.json()).not.toBeNull(),
+      (err) => fail(err)
+    )
+  }))
 });
