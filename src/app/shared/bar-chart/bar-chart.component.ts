f2410cd120fb61c152b52b6af04277d93fac4f66
Manolo <manuelgmaroto@gmail.com>
2022-02-25 11:25:57 +0100

    Added funcionality to nav bar

--- src/app/shared/bar-chart/bar-chart.component.ts
+++ src/app/shared/bar-chart/bar-chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import * as d3 from 'd3'
+import { style } from 'd3';
 import { F1StatsService } from '../../services/f1-stats.service';
 
 @Component({
@@ -15,16 +16,14 @@ export class BarChartComponent implements OnInit {
   private svg: any;
   public chartData: Array<any>= [];
 
-  private barWidth: number = 60;
-  private margin: number = 20;
+  private barWidth: number = 50;
+  private margin: number = 39;
   private width: number = 1000 - this.margin * 2;
   private height: number = 600 - this.margin * 2;
   private xAxisWidth: number;
   private yAxisHeight: number;
   private xScale: any;
   private yScale: any;
-
-  private colors: Array<string> = ['red', 'green', 'blue', 'yellow']
 
   private teamColors = [
     {team: 'Mercedes', color: '#00D2BE'},
@@ -82,13 +81,14 @@ export class BarChartComponent implements OnInit {
       pointsMax = this.chartData[0][1];
       this.chart.remove();
       this.chart = this.svg.append('g');
-      this.createAxis(this.chartData.length, pointsMax);
+      this.createAxis(this.chartData.length, pointsMax) ;
       this.createBars('constructors');
-      this.chart.attr('transform', `translate(0, ${this.margin}) scale(.6)`);
+      this.chart.attr('transform', `translate(0, ${this.margin}) scale(.8)`);
     });
   }
 
   createAxis(items: number, pointsMax) {
+
       this.xAxisWidth = (this.barWidth + this.margin) * items;
       this.yAxisHeight = pointsMax + this.margin;
 
@@ -106,6 +106,7 @@ export class BarChartComponent implements OnInit {
           .selectAll('text')
             .attr('transform', 'translate(-10, 0)rotate(-45)')
             .style('color','white')
+            .style('font-size', '18px')
             .style('text-anchor', 'end')
 
       this.chart
@@ -113,12 +114,16 @@ export class BarChartComponent implements OnInit {
           .attr('transform', `translate(${this.margin}, 0)`)
           .call(d3.axisLeft(this.yScale))
           .selectAll('text')
+            .style('font-size', '18px')
             .style('color','white');
   }
 
   createBars(item) {
     let origin;
     item == 'drivers' ? origin = 2 : origin = 0;
+    if(item=='constructors'){
+      this.barWidth = 50;
+    }
     this.chart.selectAll().data(this.chartData)
         .enter()
         .append('rect')
@@ -126,7 +131,7 @@ export class BarChartComponent implements OnInit {
         .attr('y', d => this.yAxisHeight - d[1])
         .attr('width', this.barWidth)
         .attr('height', d => d[1])
-        .style('fill', (d,i) => this.getColor(d[origin]));
+        .style('fill', (d,i) => this.getColor(d[origin]))
   }
 
   getColor(item): string{
