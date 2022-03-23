6a0d47a4296c3ca37229de55758e8b12c49954bc
Manolo <manuelgmaroto@gmail.com>
2022-03-23 10:45:49 +0100

    Fixed and improved bar chart

--- src/app/shared/bar-chart/bar-chart.component.ts
+++ src/app/shared/bar-chart/bar-chart.component.ts
@@ -16,21 +16,18 @@ export class BarChartComponent implements OnInit {
   private svg: any;
   public chartData: Array<any>= [];
 
-  private barWidth: number = 50;
-  private margin: number = 39;
-  private width: number = 1000 - this.margin * 2;
-  private height: number = 600 - this.margin * 2;
-  private xAxisWidth: number;
-  private yAxisHeight: number;
-  private xScale: any;
-  private yScale: any;
+  private margin: number = 40;
+  private width: number = 1000 - this.margin;
+  private height: number = 600 - this.margin;
+  private xScale: any = d3.scaleBand().range([0, this.width]).padding(0.4);
+  private yScale: any = d3.scaleLinear().range([this.height, 0]);
 
   private teamColors = [
     {team: 'Mercedes', color: '#00D2BE'},
     {team: 'Ferrari', color: '#DC0000'},
     {team: 'Red Bull', color: '#0600EF'},
     {team: 'Alpine F1 Team', color: '#0090FF'},
-    {team: 'Hass F1 Team', color: '#FFFFFF'},
+    {team: 'Haas F1 Team', color: '#FFFFFF'},
     {team: 'Aston Martin', color: '#006F62'},
     {team: 'AlphaTauri', color: '#2B4562'},
     {team: 'McLaren', color: '#FF8700'},
@@ -43,7 +40,7 @@ export class BarChartComponent implements OnInit {
   ngOnInit(): void {
     this.svg = d3.select("#divChart")
                 .append('svg')
-                .attr("viewBox", `0,0,${this.width+100}, ${this.height+100}`)
+                .attr("viewBox", `0,0,${this.width-300}, ${this.height+100}`)
     this.chart = this.svg.append('g');
 
     if (this.graphToShow == 'drivers') {
@@ -89,29 +86,23 @@ export class BarChartComponent implements OnInit {
 
   createAxis(items: number, pointsMax) {
 
-      this.xAxisWidth = (this.barWidth + this.margin) * items;
-      this.yAxisHeight = pointsMax + this.margin;
-
       let xDomain = this.chartData.map(d => d[0]);
-      this.xScale = d3.scaleBand()
-                      .domain(xDomain).range([0, this.xAxisWidth]);
-      let yDomain = [0, pointsMax];
-      this.yScale = d3.scaleLinear().domain(yDomain)
-                      .range([this.yAxisHeight, 0]);
-
+      
+      let yDomain = [0, +pointsMax];
+      this.xScale.domain(xDomain);
+      this.yScale.domain(yDomain)
       this.chart
           .append('g')
-          .attr('transform', `translate(${this.margin}, ${this.yAxisHeight})`)
+          .attr('transform', `translate(0, ${this.height})`)
           .call(d3.axisBottom(this.xScale))
           .selectAll('text')
-            .attr('transform', 'translate(-10, 0)rotate(-45)')
+            .attr('transform', 'translate(-10, 0)rotate(-75)')
             .style('color','white')
             .style('font-size', '18px')
             .style('text-anchor', 'end')
 
       this.chart
           .append('g')
-          .attr('transform', `translate(${this.margin}, 0)`)
           .call(d3.axisLeft(this.yScale))
           .selectAll('text')
             .style('font-size', '18px')
@@ -121,17 +112,30 @@ export class BarChartComponent implements OnInit {
   createBars(item) {
     let origin;
     item == 'drivers' ? origin = 2 : origin = 0;
-    if(item=='constructors'){
-      this.barWidth = 50;
-    }
-    this.chart.selectAll().data(this.chartData)
+    const barGroups = this.chart.selectAll().data(this.chartData)
         .enter()
-        .append('rect')
-        .attr('x', (d, i) => this.margin * 1.5 + i*(this.margin+this.barWidth))
-        .attr('y', d => this.yAxisHeight - d[1])
-        .attr('width', this.barWidth)
-        .attr('height', d => d[1])
-        .style('fill', (d,i) => this.getColor(d[origin]))
+        .append('g');
+    barGroups.append('rect')
+        .attr('x', d => this.xScale(d[0]))
+        .attr('y', d => this.yScale(d[1]))
+        .attr('width', this.xScale.bandwidth())
+        .attr('height', d => this.height - this.yScale(d[1]));
+    // Add some efects and color
+    barGroups.on('mouseenter', function (actual, i) {
+          d3.select(this).attr('opacity', 0.7)
+        })
+        .on('mouseleave', function (actual, i) {
+          d3.select(this).attr('opacity', 1)
+        })
+        .style('fill', (d,i) => this.getColor(d[origin]));
+    // Add text with points in the bars
+    barGroups.append('text')
+        .text( d => d[1])
+        .attr('x', d => this.xScale(d[0]) + this.xScale.bandwidth()/2)
+        .attr('y', d => this.yScale(d[1]) -5 )
+        .attr("font-size" , "20px")
+        .attr('fill', 'white')
+        .attr("text-anchor", "middle");
   }
 
   getColor(item): string{
